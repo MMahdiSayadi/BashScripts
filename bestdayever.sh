33f58d379e8e0a95716fa910309f6b89140b2aa0
M.Mahdi Sayadi <80895134+mohammadmahdisayadi1374@users.noreply.github.com>
2022-08-07 19:50:04 +0430

    Add files via upload

---
+++ bestdayever.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+name=$1
+compliment=$2
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
+
+echo "good morning $name!!"
+sleep 1
+echo "you are looking good today $name!!"
+sleep 1
+echo "You have the best $compliment I've ever seen $name!!"
+sleep 2
+echo "You are currently are logged in as $user and you are in the directory $whereami. Also today is : $date"
+
+
